0eb568b752ee09714d8caa3cc225ec83db1a62ff
bellmin <bellmin9321@gmail.com>
2021-10-22 21:22:31 +0900

    add morgan, helmet 설명

---
+++ 7-cors/app-cors.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+
+const app = express();
+
+const corsOption = {
+  origin: ['http://127.0.0.1:5500'],
+    optionsSuccessStatus: 200,
+    credentials: true, // Access-Control-Allow-Credentials: true
+}
+// 유용한 Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('tiny'));
+app.use(cors(corsOption));
+app.use(helmet());
+
+app.get('/', (req, res) => {
+  console.log(req.body);
+  console.log(req.cookies);
+  // req.cookies.yummy_cookie;
+  res.send('Welcome!');
+});
+
+
+app.listen(8080);
