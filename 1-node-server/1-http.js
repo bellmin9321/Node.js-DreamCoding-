17fa0c0e62b6fccb52130e95c6259bd96ee03894
bellmin <bellmin9321@gmail.com>
2021-10-22 21:53:25 +0900

    making node-server

---
+++ 1-node-server/1-http.js
@@ -0,0 +1,32 @@
+const http = require('http');
+// const http2 = require('http2'); // https
+
+// console.log(http.STATUS_CODES);
+// console.log(http.METHODS);
+
+const server = http.createServer((req, res) => {
+  console.log('incoming...');
+  console.log(req.headers);
+  console.log(req.httpVersion);
+  console.log(req.method);
+  console.log(req.url);
+  const url = req.url;
+  if(url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Academy</title></head>');
+    res.write('<body><h1>Welcome!</h1></body>');
+    res.write('</html>');
+  } else if (url === '/courses') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Hello</title></head>');
+    res.write('<body><h1>Courses!</h1></body>');
+    res.write('</html>');
+  } else {
+    res.write('<h1>Not found</h1>')
+  }
+  res.end();
+})
+
+server.listen(8080);
