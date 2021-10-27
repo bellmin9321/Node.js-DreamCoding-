295e438a9115c71694f1b51da3bb5ed85bffac40
bellmin <bellmin9321@gmail.com>
2021-10-27 17:21:56 +0900

    workingDir 안에 분류 폴더 만들기

--- 0-project/main.js
+++ 0-project/main.js
@@ -1,18 +1,24 @@
 // 계획
-// 1. 사용자가 원하는 폴더의 이름을 받아온다
-// 2. 그 폴더안에 video, captured, duplicated 폴더를 만든다.
-// 3. 폴더안에 있는 파일들을 다 돌면서 해당하는 mp4|mov, png|aae, IMG_1234(IMG_E1234)
 
-// console.log(process.argv[2]);
+// 1. 사용자가 원하는 폴더의 이름을 받아온다
 const path = require('path');
+const process = require('process');
 const os = require('os');
 const fs = require('fs');
 
 const folder = process.argv[2];
+// Pictures 라는 폴더 안에서 작업
 const workingDir = path.join(os.homedir(), 'Pictures', folder);
-if(!folder || !fs.existsSync(workingDir)) {
-  console.error('Please enter folder name in pictures');
+if (!folder || !fs.existsSync(workingDir)) {
+  console.error('Please enter folder name in Pictures');
   return;
 }
 
-console.log(workingDir);
+// 2. 그 폴더안에 video, captured, duplicated 폴더를 만든다.
+const videoDir = path.join(workingDir, 'video');
+const capturedDir = path.join(workingDir, 'captured');
+const duplicatedDir = path.join(workingDir, 'duplicated');
+
+console.log(videoDir);
+console.log(capturedDir);
+console.log(duplicatedDir);
