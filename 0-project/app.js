1ee9ee9ac3307d247667f9c1666572467df4aa87
bellmin <bellmin9321@gmail.com>
2021-10-28 23:03:23 +0900

    혼자 파일정리 프로그램 만들어보기 시작!

--- 0-project/app.js
+++ 0-project/app.js
@@ -1,33 +1,39 @@
 const fs = require('fs');
 const path = require('path')
 
-function mkdir (dirPath) {
-  const isExists = fs.existsSync(dirPath);
-  if (!isExists) {
-    fs.mkdirSync( dirPath, { recursive: true});
-  }
+// 작업 폴더 설정
+const folder = 'pics';
+const usingDir = path.join(__dirname, folder);
+if (!folder || !fs.existsSync(usingDir)) {
+  console.error('작업폴더가 아니야');
+  return;
 }
 
-mkdir('./test/captured');
-// mkdir('./test/duplicated');
-// mkdir('./test/video');
-const currentPath = path.join(__dirname, "test", "jpg");
-const newPath = path.join(__dirname, "test/captured", "jpg");
+// video, duplicated, captured 폴더 생성
+const videoDir = path.join(__dirname, 'video');
+const duplicatedDir = path.join(__dirname, 'duplicated');
+const capturedDir = path.join(__dirname, 'captured');
 
-fs.rename(currentPath, newPath, function(err) {
-  if (err) {
-    throw err 
-  } else {
-    console.log("png file moved in captured");
-  }
-});
+if(!fs.existsSync(videoDir)) {
+  fs.mkdir(videoDir)
+  console.log('video dir');
+}
+// const videoDir = fs.mkdir(path.join(__dirname, 'video'), (e) => {
+//   if(!path.existsSync(videoDir)) {
+//     !videoDir
+//   }
+//   console.log('video 폴더 생성');
+// })
 
-console.log(__dirname);
 
-// rename 이용
-// try {
-//   fs.renameSync(currentPath, newPath)
-//   console.log("Successfully moved the file!")
-// } catch(err) {
-//   throw err
+
+// ---------------------------------------------------------
+// function mkdir (dirPath) {
+//   const isExists = fs.existsSync(dirPath);
+//   if (!isExists) {
+//     fs.mkdirSync( dirPath, { recursive: true});
+//   }
 // }
+
+// const png = path.extname('captured.png')
+// console.log(png);
